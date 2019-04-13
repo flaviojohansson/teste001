9dcc5c7391a840dad9af4645c0a942aea42628fd
Flavio Johansson <flavio@indiepine.com>
2019-04-13 00:21:07 -0300

    Adicionar varias coisas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Nav from './components/nav';
+
 import Landing from './components/landing';
+import Signin from './components/signin';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-      <div className="App">
-        <Nav />
-        <main className="App-header">
-          <Switch>
-            <Route path="/" exact={true} component={Landing} />
-          </Switch>        
-        </main>
-      </div>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Nav />
+                    <main className="App-header">
+                        <Switch>
+                            <Route path="/" exact component={Landing} />
+                            <Route path="/signin" component={Signin} />
+                            <Route render={() => <h1>Not found</h1>}/>
+                        </Switch>
+                    </main>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {
+    Navbar,
+    Nav,
+    Container,
+} from 'react-bootstrap'
+
+const links = [
+    { href: '/', label: 'Home' },
+    { href: '/signin', label: 'Signin' },
+]
 
 const nav = () => {
   return (
-    <React.Fragment>
-      <h1>Nav Bar</h1>
-      <NavLink to="/">Home</NavLink>
-    </React.Fragment>
+    <Navbar collapseOnSelect bg="light" variant="light" expand="md" sticky="top">
+        <Container>
+            <NavLink to="/">
+                <Navbar.Brand to="/">
+                    <div>Hello Bill</div>
+                </Navbar.Brand>
+            </NavLink>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto bold">
+                    {links.map((link, idx) => {
+                        return (
+                            <NavLink
+                                exact
+                                key={idx}
+                                to={link.href}
+                                activeClassName="active-link">
+                                {link.label}
+                            </NavLink>
+                        )
+                    })}
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
   )
 }
 
